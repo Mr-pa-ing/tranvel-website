48cdd1a3a0b73506f82fb131ac04b3604cb0a0a2
Mr-pa-ing <paingphyozaw54@gmail.com>
2021-12-11 11:12:57 +0630

    Navber Section

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// START NAVBAR
+const searchinput = document.querySelector('.searchinputs');
+const searchbtn = document.querySelector('.searchbtns');
+searchbtn.addEventListener('click',()=>{
+    searchinput.classList.toggle("searchinputactives");
+})
+// END NAVBAR
