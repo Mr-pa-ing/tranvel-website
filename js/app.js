572c0e79fa317a16f2f281607fb18af12eec8b14
Mr-pa-ing <paingphyozaw54@gmail.com>
2021-12-12 11:59:13 +0630

    add two sections

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@ searchbtn.addEventListener('click',()=>{
     searchinput.classList.toggle("searchinputactives");
 })
 // END NAVBAR
+
+// START DESTINATION 
+const destilis = document.querySelectorAll('.destilis');
+destilis.forEach(destili =>{
+    destili.addEventListener('click',(e)=>{
+        if(!e.target.classList.contains('active')){
+            removecurrentactive();
+            e.target.classList.add('active');
+        }
+    });
+});
+function removecurrentactive(){
+    destilis.forEach(destili=>{
+        destili.classList.remove('active');
+    })           
+}
+// END DESTINATION 
